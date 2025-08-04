73b84277360da9370aecd26392a2621b6c8d8259
Ilya <82446027+ko0g@users.noreply.github.com>
2025-08-04 19:18:01 +0300

    backend setup
    backend part was added and frontend was modified a bit

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,5 @@
-const API_KEY = '11f01e0a-ec02-4d5e-aeb2-76e2f4e6efca';
+const API_KEY_MAP = '1bc282243dca48c0acd5582d97d4e00a';
+const API_KEY_ROUTE = '11f01e0a-ec02-4d5e-aeb2-76e2f4e6efca';
 
 window.onload = () => {
     const map = new ol.Map({
@@ -6,8 +7,10 @@ window.onload = () => {
         layers: [
             new ol.layer.Tile({
                 source: new ol.source.XYZ({
-                    url: 'https://{a-c}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png/{z}/{x}/{y}.png',
-                    attributions: '© OpenStreetMap contributors, © CartoDB'
+                    url: `https://maps.geoapify.com/v1/tile/klokantech-basic/{z}/{x}/{y}.png?&apiKey=${API_KEY_MAP}`,
+                    attributions: 'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | <a href="https://openmaptiles.org/" target="_blank">© OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap</a> contributors',
+                    maxZoom: 20,
+                    minZoom: 8
                 })
             })
         ],
@@ -16,14 +19,12 @@ window.onload = () => {
             zoom: 15
         })
     });
+    const mainSource = new ol.source.Vector();
+    const routeSource = new ol.source.Vector();
 
-    const vectorSource1 = new ol.source.Vector();
-    const vectorLayer1 = new ol.layer.Vector({
-        source: vectorSource1
+    const mainLayer = new ol.layer.Vector({
+        source: mainSource
     });
-    map.addLayer(vectorLayer1);
-
-    /*const routeSource = new ol.source.Vector();
     const routeLayer = new ol.layer.Vector({
         source: routeSource,
         style: new ol.style.Style({
@@ -34,47 +35,52 @@ window.onload = () => {
             })
         })
     });
+
     map.addLayer(routeLayer);
-
-    const point_precision = 100000;
-
+    map.addLayer(mainLayer);
 
     ready = false;
-
-    const info_txt = document.getElementById('info');
 
     start = [];
     points = [];
 
+    const colors = ['red', 'orange', 'blue', 'cyan', 'purple'];
+
     map.on('click', function (event) {
         const coordinate = event.coordinate;
         const lonLat = ol.proj.toLonLat(coordinate);
-        const pointFeature = new ol.Feature({
-            geometry: new ol.geom.Point(event.coordinate)
-        });
-        let color;
-        if (!ready){
-            color = 'green';
-            start = [+lonLat[0].toFixed(6), +lonLat[1].toFixed(6)];
+
+        if (!ready) {
+            start = [lonLat[0].toFixed(6), lonLat[1].toFixed(6)];
             ready = true;
-            info_txt.innerHTML = `Стартовая точка: ${start[1]} ${start[0]}`;
+            color = 'green';
+        } else {
+            color = colors[points.length % colors.length];
+            points.push([lonLat[0].toFixed(6), lonLat[1].toFixed(6)]);
         }
-        else{
-            color = 'red';
-            points.push([+lonLat[0].toFixed(6), +lonLat[1].toFixed(6)]);
-        }
+
+        const svg = `
+            <svg width="31" height="45" viewBox="0 0 31 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15.1767 0.000305176C23.7288 0.361278 30.5118 7.26639 30.1578 15.9542C30.0356 18.9539 28.6344 21.91 27.0039 24.4072C26.398 25.44 25.8375 26.5003 25.2476 27.5424L22.3245 32.705C21.9193 33.4368 21.264 34.2909 21.0211 35.077C20.4553 35.6326 18.7421 38.6716 18.3242 39.4428C17.5467 40.8855 15.7605 43.3873 15.2669 44.7889L15.1767 44.7189C15.2449 44.1851 15.1797 43.5783 15.1787 43.0349L15.1752 23.5193C15.173 23.0735 15.1124 22.5742 15.1767 22.1372C19.0375 22.0344 22.1771 19.0368 22.2234 15.1181C22.2687 11.2739 18.9718 8.11345 15.1767 8.0872V0.000305176Z" fill="${color}"/>
+            <path d="M3.1235 24.4072C1.60028 20.7888 -0.0123236 19.3512 7.09993e-05 14.798C0.0220896 6.69111 7.12475 -0.0506043 15.1767 0.000286242V8.08718C18.9718 8.11343 22.2687 11.2739 22.2234 15.1181C22.1771 19.0368 19.0375 22.0344 15.1767 22.1372C15.1124 22.5742 15.173 23.0735 15.1752 23.5193L15.1787 43.0349C15.1798 43.5783 15.2449 44.1851 15.1767 44.7189C14.5053 43.6151 13.72 42.4739 13.0243 41.3552C12.6339 40.7274 12.3099 40.0492 11.8883 39.4428C11.2828 38.2355 9.98979 36.1851 9.21491 35.077C7.39583 31.7829 5.3586 28.621 3.59872 25.2885C3.45086 24.9872 3.29396 24.6962 3.1235 24.4072ZM15.1767 8.08718C11.2893 8.07858 8.01118 11.2014 7.99952 15.1181C7.98742 19.1265 11.2839 22.0469 15.1767 22.1372C19.0375 22.0344 22.1771 19.0368 22.2234 15.1181C22.2687 11.2739 18.9718 8.11343 15.1767 8.08718Z" fill="${color}"/>
+            </svg>
+        `;
+        const encoded_svg = 'data:image/svg+xml;utf8,' + encodeURIComponent(svg);
+
+        const pointFeature = new ol.Feature({
+            geometry: new ol.geom.Point(coordinate)
+        });
         pointFeature.setStyle(new ol.style.Style({
-            image: new ol.style.Circle({
-                radius: 3,
-                fill: new ol.style.Fill({ color }),
-                stroke: new ol.style.Stroke({ color: '#000', width: 2 })
+            image: new ol.style.Icon({
+                anchor: [0.5, 1],
+                src: encoded_svg,
+                scale: 0.9
             })
         }));
-        vectorSource1.addFeature(pointFeature);
+        mainSource.addFeature(pointFeature);
     });
 
-    const calcbtn = document.getElementById('calcbutton');
-
+    const point_precision = 100000;
     function decodePolyline(encoded) {
         const points = [];
         let index = 0, lat = 0, lng = 0;
@@ -105,8 +111,10 @@ window.onload = () => {
         return points;
     }
 
+    const calcbtn = document.getElementById('btn');
+
     function calc(toPoint, curColor) {
-        const url = `https://graphhopper.com/api/1/route?point=${start[1]},${start[0]}&point=${toPoint[1]},${toPoint[0]}&vehicle=foot&locale=ru&key=${API_KEY}`;
+        const url = `https://graphhopper.com/api/1/route?point=${start[1]},${start[0]}&point=${toPoint[1]},${toPoint[0]}&vehicle=foot&locale=ru&key=${API_KEY_ROUTE}`;
         fetch(url)
             .then(res => res.json())
             .then(data => {
@@ -128,17 +136,15 @@ window.onload = () => {
                     console.error('Маршрут не найден.');
                 }
             })
-            .catch(err => {
-                console.error('Ошибка при получении маршрута:', err);
-            });
+        .catch(err => {
+            console.error('Ошибка при получении маршрута:', err);
+        });
     }
 
-    const colors = ['red', 'green', 'blue', 'cyan', 'purple'];
-
     calcbtn.onclick = () => {
-        routeSource.clear(); // очищаем старые маршруты
+        routeSource.clear();
         for (let i = 0; i < points.length; i++) {
             calc(points[i], colors[i % colors.length]);
         }
-    };*/
+    };
 };
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Статическая папка
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+// Отдать index.html по корню
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'main.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен: http://localhost:${PORT}`);
+});
